0fb7132a43fd6e878a9c3f4b913be8da85c6fbdd
Edsoncontreras7 <econtrerasl3@miumg.edu.gt>
2025-08-22 16:06:16 -0600

    creo que asi quedarian los controladores, le puse backen a la raiz de la ruta, asi al buscar ya solo seria de agregar la entidad despues de la raiz!

--- backend/src/main/java/proyecto/backend/model/controller/EmpresaController.java
+++ backend/src/main/java/proyecto/backend/model/controller/EmpresaController.java
@@ -1,5 +1,63 @@
 package proyecto.backend.model.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import proyecto.backend.model.entity.Empresa;
+import proyecto.backend.model.service.EmpresaService;
+import java.util.List;
+
+@RestController
+@RequestMapping("/backend/empresas")
+
 public class EmpresaController {
 
+    private final EmpresaService service;
+
+    public EmpresaController(EmpresaService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Empresa>> getAllEmpresas() {
+        List<Empresa> empresas = service.findAll();
+        return ResponseEntity.ok(empresas);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Empresa> getEmpresaById(@PathVariable Integer id) {
+        Empresa empresa = service.findById(id);
+        if (empresa != null) {
+            return ResponseEntity.ok(empresa);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Empresa> createEmpresa(@RequestBody Empresa e) {
+        Empresa nuevaEmpresa = service.create(e);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevaEmpresa);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Empresa> updateEmpresa(@PathVariable Integer id, @RequestBody Empresa e) {
+        Empresa actualizada = service.update(id, e);
+        if (actualizada != null) {
+            return ResponseEntity.ok(actualizada);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEmpresa(@PathVariable Integer id) {
+        Empresa empresa = service.findById(id);
+        if (empresa != null) {
+            service.delete(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
